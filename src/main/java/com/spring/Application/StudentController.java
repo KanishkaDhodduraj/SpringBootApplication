941dcdff028fbccf47e29cfd2d5f622efef52eff
Kanishka Dhodduraj <kanishkadhodduraj2007@gmail.com>
2026-08-09 12:25:30 +0530

    feat: implement student REST API

---
+++ src/main/java/com/spring/Application/StudentController.java
@@ -0,0 +1,39 @@
+package com.spring.Application;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/student")
+
+public class StudentController {
+
+    @GetMapping("/getId")
+    String getStudentId() {
+        return "Student ID";
+    }
+
+    @GetMapping("/{id}")
+    String getId(@PathVariable int id){
+        return("Student ID: " + id);
+    }
+
+    @GetMapping
+    String getSID(@RequestParam("SID") int id){
+        return("Student ID: " + id);
+    }
+
+    @PostMapping("/create")
+    String createStudent(@RequestBody String body){
+        return body;
+    }
+
+    @PutMapping("/{id}")
+    String updateStudentById(@PathVariable int id){
+        return("Update Student ID: " + id);
+    }
+
+    @DeleteMapping("/{id}")
+    String deleteStudentById(@PathVariable int id){
+        return("Delete Student ID: " + id);
+    }
+}
